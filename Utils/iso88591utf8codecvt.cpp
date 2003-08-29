b567522d6d349ca5763c34f635d308059b5a952c
jez_higgins <>
2003-08-29 13:45:53 +0000

    *** empty log message ***

---
+++ Utils/iso88591utf8codecvt.cpp
@@ -0,0 +1,60 @@
+//---------------------------------------------------------------------------
+// $Id$
+//---------------------------------------------------------------------------
+#include "iso88591utf8codecvt.h"
+#include "impl/iso88591_utf8.h"
+//---------------------------------------------------------------------------
+// This facet converts from ISO8859:1 (Latin 1) chars to UTF-8 encoded chars.
+
+std::codecvt_base::result iso88591utf8codecvt::do_out(std::mbstate_t& /* state */,
+                        const char* from,
+                        const char* from_end,
+                        const char*& from_next,
+                        char* to,
+                        char* to_limit,
+                        char*& to_next) const
+{
+  return ArabicaInternal::utf8_2_iso88591(from, from_end, from_next, to, to_limit, to_next);
+} // do_out
+
+std::codecvt_base::result iso88591utf8codecvt::do_in(std::mbstate_t& /* state */,
+                       const char* from,
+                       const char* from_end,
+                       const char*& from_next,
+                       char* to,
+                       char* to_limit,
+                       char*& to_next) const
+{
+  return ArabicaInternal::iso88591_2_utf8(from, from_end, from_next, to, to_limit, to_next);
+} // do_in
+
+std::codecvt_base::result iso88591utf8codecvt::do_unshift(std::mbstate_t& /* state */,
+                            char* to,
+                            char* /* to_limit */,
+                            char*& to_next) const
+{
+  to_next = to;
+  return noconv;
+} // do_unshift
+
+int iso88591utf8codecvt::do_length(const std::mbstate_t&,
+                        const char* from,
+                        const char* end,
+                        size_t max) const
+{
+  size_t count(0);
+  const char* from_next = from;
+
+  while((from_next < end) && (count < max))
+  {
+    unsigned char fn = static_cast<unsigned char>(*from_next);
+    if(fn && 0x80)
+      ++count;
+    ++count;
+    ++from_next;
+  } // while
+
+  return (from_next-from);
+} // do_length
+
+// end of file
---
+++ Utils/utf8iso88591codecvt.h
@@ -0,0 +1,49 @@
+#ifndef ARABICA_UTF8ISO88591_CODECVT_H
+#define ARABICA_UTF8ISO88591_CODECVT_H
+#include <locale>
+
+#if(_MSC_VER < 1300)
+namespace std {
+  typedef ::mbstate_t mbstate_t;
+}
+#endif
+
+class utf8iso88591codecvt : public std::codecvt<char, char, std::mbstate_t>
+{
+protected:
+  virtual ~utf8iso88591codecvt() { }
+
+  virtual result do_out(std::mbstate_t&,
+                        const char* from,
+                        const char* from_end,
+                        const char*& from_next,
+                        char* to,
+                        char* to_limit,
+                        char*& to_next) const;
+
+  virtual result do_in(std::mbstate_t&,
+                       const char* from,
+                       const char* from_end,
+                       const char*& from_next,
+                       char* to,
+                       char* to_limit,
+                       char*& to_next) const;
+
+  virtual result do_unshift(std::mbstate_t&,
+                            char*,
+                            char*,
+                            char*&) const;
+
+  virtual int do_encoding() const throw() { return 0; }
+
+  virtual bool do_always_noconv() const throw() { return false; }
+
+  virtual int do_length(const std::mbstate_t&,
+                        const char* from,
+                        const char* end,
+                        size_t max) const;
+
+  virtual int do_max_length() const throw() { return 2; }
+}; // class utf8iso88591codecvt
+
+#endif
