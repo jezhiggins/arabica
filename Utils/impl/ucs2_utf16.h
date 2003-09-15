117190ebd89c978d57253b10fdb08ffed7fe0856
jez_higgins <>
2003-09-15 10:03:41 +0000

    *** empty log message ***

--- Utils/impl/ucs2_utf16.h
+++ Utils/impl/ucs2_utf16.h
@@ -8,10 +8,12 @@ namespace Arabica
 namespace Internal 
 {
 
-std::codecvt_base::result ucs2_2_utf16(bool be, const wchar_t* from, const wchar_t* from_end, const wchar_t*& from_next,
-                                      char* to, char* to_limit, char*& to_next);
-std::codecvt_base::result utf16_2_ucs2(bool be, const char* from, const char* from_end, const char*& from_next,
-                                      wchar_t* to, wchar_t* to_limit, wchar_t*& to_next);
+std::codecvt_base::result ucs2_2_utf16(bool be, 
+                                       wchar_t const* from, wchar_t const* from_end, wchar_t const*& from_next,
+                                       char* to, char* to_limit, char*& to_next);
+std::codecvt_base::result utf16_2_ucs2(bool be, 
+                                       char const* from, char const* from_end, char const*& from_next,
+                                       wchar_t* to, wchar_t* to_limit, wchar_t*& to_next);
 
 } // namepsace Internal
 } // namespace Arabica
