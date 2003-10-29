df3d61ce712298d75d1d975174cee59acba8b41c
jez_higgins <>
2003-10-29 15:51:21 +0000

    An ErrorHandler implementation that grabs hold of any errors reported, so you can check later to see if anything happened.

---
+++ SAX/helpers/CatchErrorHandler.h
@@ -0,0 +1,51 @@
+#ifndef ARABICA_CATCH_ERROR_HANDLER_H
+#define ARABICA_CATCH_ERROR_HANDLER_H
+
+#include <SAX/ErrorHandler.h>
+
+/**
+ * An ErrorHandler implementation that keeps hold of any errors for later reporting.
+ */
+
+namespace SAX
+{
+template<class string_type>
+class CatchErrorHandler : public SAX::basic_ErrorHandler<string_type>
+{
+public:
+  typedef SAX::basic_SAXParseException<string_type> SAXParseExceptionT;
+
+  CatchErrorHandler() : errors_() { }
+  virtual ~CatchErrorHandler() { }
+
+  virtual void warning(const SAXParseExceptionT& exception)
+  {
+    hold(exception);
+  } // warning
+
+  virtual void error(const SAXParseExceptionT& exception)
+  {
+    hold(exception);
+  } // error
+
+  virtual void fatalError(const SAXParseExceptionT& exception)
+  {
+    hold(exception);
+  } // fatalError
+
+  bool errorsReported() const { return !errors_.empty(); }
+  const std::string& errors() const { return errors_; }
+  void reset() { errors_.clear(); }
+
+private:
+  void hold(const SAXParseExceptionT& exception)
+  {
+    errors_ += exception.what();
+  } // hold
+
+  std::string errors_;
+}; // class CatchErrorHandler
+
+} // namespace Arabica
+
+#endif
