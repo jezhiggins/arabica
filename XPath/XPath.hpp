65675f5d2f9637139f712f3f06cbcae6735561cb
jez_higgins <>
2005-08-16 17:55:25 +0000

    *** empty log message ***

--- XPath/XPath.hpp
+++ XPath/XPath.hpp
@@ -9,4 +9,31 @@
 #include <XPath/impl/xpath_logical.hpp>
 #include <XPath/impl/xpath_arithmetic.hpp>
 
+/* To parameterise
+
+DONE - xpath_arithmetic.hpp
+xpath_ast.hpp
+DONE - xpath_ast_ids.hpp
+xpath_axis_enumerator.hpp
+xpath_compile_context.hpp
+xpath_execution_context.hpp
+xpath_expression.hpp
+xpath_function.hpp
+xpath_function_holder.hpp
+xpath_function_resolver.hpp
+xpath_grammar.hpp
+xpath_logical.hpp
+xpath_namespace_context.hpp
+xpath_namespace_node.hpp
+xpath_node_test.hpp
+xpath_object.hpp
+xpath_parser.hpp
+xpath_relational.hpp
+xpath_resolver_holder.hpp
+xpath_step.hpp
+xpath_union.hpp
+xpath_value.hpp
+xpath_variable.hpp
+xpath_variable_resolver.hpp
+*/
 #endif
