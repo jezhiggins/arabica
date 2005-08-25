d7c23a30b833c0f5ee47e7802383ec3c568618a2
jez_higgins <>
2005-08-25 11:31:09 +0000

    big time changes to stream namespaces and namespace-prefixes properly not quite done yet, but much improved

--- DOM/Utils/Stream.h
+++ DOM/Utils/Stream.h
@@ -101,6 +101,143 @@ namespace DOM
   
   } // namespace StreamImpl
 
+template<class stringT>
+void prefix_mapper_callback(std::ios_base::event ev, std::ios_base& stream, int index)
+{
+  if(ev == std::ios_base::erase_event)
+    delete static_cast<std::vector<std::map<stringT, stringT> >*>(stream.pword(index));
+} // boing_callback
+
+template<class stringT>
+std::pair<bool, stringT> is_uri_declared(std::vector<std::map<stringT, stringT> >* prefix_stack, const stringT& namespaceURI)
+{
+  bool declared = false;
+  stringT declared_prefix;
+  for(std::vector<std::map<stringT, stringT> >::reverse_iterator b = prefix_stack->rbegin(), e = prefix_stack->rend();
+        b != e;
+        ++b)
+  {
+    std::map<stringT, stringT>::const_iterator p = b->find(namespaceURI);
+    if(p != b->end())
+      return std::make_pair(true, p->second);
+  } // for ...
+
+  return std::make_pair(false, stringT());
+} // prefix_is_declared
+
+template<class stringT, class charT, class traitsT>
+void check_and_output_node_name(std::basic_ostream<charT, traitsT>& stream, 
+                                DOM::Node<stringT>& node, 
+                                std::vector<std::map<stringT, stringT> >* prefix_stack)
+{
+  std::map<stringT, stringT>& current = *(prefix_stack->rbegin());
+
+  stringT namespaceURI = node.getNamespaceURI();
+  if(!namespaceURI.empty())
+  {
+    std::pair<bool, stringT> prefix = is_uri_declared(prefix_stack, namespaceURI);
+    
+    if(!prefix.first)
+      current[namespaceURI] =  prefix.second = node.getPrefix();
+
+    if(!prefix.second.empty())
+      stream << prefix.second << Arabica::Unicode<charT>::COLON;
+    stream << node.getLocalName();
+  }
+  else
+    stream << node.getNodeName();
+} // check_and_output_node_name
+
+template<class stringT, class charT, class traitsT>
+int prefix_mapper(std::basic_ostream<charT, traitsT>& stream,
+                   DOM::Node<stringT>& node)
+{
+  typedef Arabica::Unicode<charT> UnicodeT;
+
+  static const int index = std::ios_base::xalloc();
+  std::vector<std::map<stringT, stringT> >* prefix_stack;
+  
+  if(stream.pword(index) != 0)
+  {
+    prefix_stack = static_cast<std::vector<std::map<stringT, stringT> >*>(stream.pword(index));
+    prefix_stack->push_back(std::map<stringT, stringT>());
+  }
+  else
+  {
+    prefix_stack = new std::vector<std::map<stringT, stringT> >;
+    stream.pword(index) = prefix_stack;
+
+    std::map<stringT, stringT> prefixes;
+    for(DOM::Node<stringT> p = node.getParentNode(); p.getNodeType() == DOM::Node_base::ELEMENT_NODE; p = p.getParentNode())
+    {
+      if(p.getNamespaceURI().empty())
+        continue;
+      if(prefixes.find(p.getNamespaceURI()) == prefixes.end())
+        prefixes[p.getNamespaceURI()] = p.getPrefix();
+    } // for ...
+
+    prefix_stack->push_back(prefixes);
+  } // if ...
+
+  std::map<stringT, stringT>& current = *(prefix_stack->rbegin());
+
+  // is element namespace URI declared?
+  check_and_output_node_name(stream, node, prefix_stack);
+  
+  DOM::NamedNodeMap<stringT> attrs = node.getAttributes();
+  for(unsigned int a = 0; a < attrs.getLength(); ++a)
+  {
+    DOM::Node<stringT> attr = attrs.item(a);
+    if((attr.getNodeName() == "xmlns") || 
+       (attr.getPrefix() == "xmlns"))
+      continue;
+    stream << UnicodeT::SPACE;
+    check_and_output_node_name(stream, attr, prefix_stack);
+    stream  << UnicodeT::EQUALS_SIGN
+            << UnicodeT::QUOTATION_MARK;
+    stringT value = attr.getNodeValue();
+    std::for_each(value.begin(), value.end(), StreamImpl::escaper<charT, traitsT>(stream));
+    stream << UnicodeT::QUOTATION_MARK;
+  }
+
+  for(std::map<stringT, stringT>::const_iterator i = current.begin(), e = current.end();
+      i != e;
+      ++i)
+  {
+    stream << UnicodeT::SPACE
+           << UnicodeT::LOWERCASE_X
+           << UnicodeT::LOWERCASE_M
+           << UnicodeT::LOWERCASE_L
+           << UnicodeT::LOWERCASE_N
+           << UnicodeT::LOWERCASE_S;
+    if(!(i->second.empty()))
+      stream << UnicodeT::COLON << i->second;
+    stream << UnicodeT::EQUALS_SIGN << UnicodeT::QUOTATION_MARK;
+    std::for_each(i->first.begin(), i->first.end(), StreamImpl::escaper<charT, traitsT>(stream));
+    stream << UnicodeT::QUOTATION_MARK;
+  } // for ...
+
+  return index;
+} // prefix_mapper
+
+template<class stringT, class charT, class traitsT>
+void prefix_mapper_pop(std::basic_ostream<charT, traitsT>& stream,
+                       DOM::Node<stringT> node,
+                       int index)
+{
+  std::vector<std::map<stringT, stringT> >* prefix_stack = 
+    static_cast<std::vector<std::map<stringT, stringT> >*>(stream.pword(index));
+
+  check_and_output_node_name(stream, node, prefix_stack);
+
+  prefix_stack->pop_back();
+  if(prefix_stack->empty())
+  {
+    delete static_cast<std::vector<std::map<stringT, stringT> >*>(stream.pword(index));
+    stream.pword(index) = 0;
+  } // if ...
+} // prefix_mapper_pop
+
 template<class stringT, class charT, class traitsT>
 std::basic_ostream<charT, traitsT>&
 operator<<(std::basic_ostream<charT, traitsT>& stream,
@@ -137,22 +274,13 @@ operator<<(std::basic_ostream<charT, traitsT>& stream,
     break;
   case DOM::Node<stringT>::ELEMENT_NODE:
     {
-      stream << UnicodeT::LESS_THAN_SIGN << node.getNodeName();
-      DOM::NamedNodeMap<stringT> attrs = node.getAttributes();
-      for(unsigned int a = 0; a < attrs.getLength(); ++a)
-      {
-        DOM::Node<stringT> attr = attrs.item(a);
-        stream << UnicodeT::SPACE 
-               << attr.getNodeName()
-               << UnicodeT::EQUALS_SIGN
-               << UnicodeT::QUOTATION_MARK;
-        stringT value = attr.getNodeValue();
-        std::for_each(value.begin(), value.end(), StreamImpl::escaper<charT, traitsT>(stream));
-        stream << UnicodeT::QUOTATION_MARK;
-      }
+      stream << UnicodeT::LESS_THAN_SIGN;
+      int index = prefix_mapper(stream, node);
       stream << UnicodeT::GREATER_THAN_SIGN;
       StreamImpl::streamChildren(stream, node);
-      stream << UnicodeT::LESS_THAN_SIGN << UnicodeT::SLASH << node.getNodeName() << UnicodeT::GREATER_THAN_SIGN;
+      stream << UnicodeT::LESS_THAN_SIGN << UnicodeT::SLASH;
+      prefix_mapper_pop(stream, node, index);
+      stream << UnicodeT::GREATER_THAN_SIGN;
     }
     break;
   case DOM::Node<stringT>::TEXT_NODE:
