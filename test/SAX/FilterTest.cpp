d5af0e27b18b127038bc19dac8d2a945f3d7b613
jez_higgins <>
2006-07-19 21:57:53 +0000

    off we go

---
+++ test/SAX/FilterTest.cpp
@@ -0,0 +1,14 @@
+#ifdef _MSC_VER
+#pragma warning(disable : 4786)
+#endif
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "tests go here" << std::endl;
+	
+	return 0;
+} // main
+
+// end of file
