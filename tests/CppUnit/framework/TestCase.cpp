07b7e8e9bbdd57da597ddcac17e3c2ffa155cb83
jez <>
2007-08-29 02:01:08 +0000

    disable VS8 warning about unsafe library functions - in this case sprinf

--- tests/CppUnit/framework/TestCase.cpp
+++ tests/CppUnit/framework/TestCase.cpp
@@ -1,3 +1,6 @@
+#ifdef _MSC_VER
+#pragma warning(disable: 4996)
+#endif
 
 #include <exception>
 #include <stdexcept>
--- tests/CppUnit/textui/TextTestResult.cpp
+++ tests/CppUnit/textui/TextTestResult.cpp
@@ -1,4 +1,7 @@
 
+#ifdef _MSC_VER
+#pragma warning(disable: 4996)
+#endif
 
 #include "TextTestResult.h"
 #include "../framework/CppUnitException.h"
