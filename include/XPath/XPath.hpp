2a61c8f9dfaac721d1daa466159c90514d467a45
jez <>
2007-09-11 08:44:00 +0000

    removed redundant includes

--- include/XPath/XPath.hpp
+++ include/XPath/XPath.hpp
@@ -2,11 +2,5 @@
 #define ARABICA_XPATHIC_XPATH_HPP
 
 #include <XPath/impl/xpath_parser.hpp>
-#include <XPath/impl/xpath_value.hpp>
-#include <XPath/impl/xpath_function.hpp>
-#include <XPath/impl/xpath_step.hpp>
-#include <XPath/impl/xpath_relational.hpp>
-#include <XPath/impl/xpath_logical.hpp>
-#include <XPath/impl/xpath_arithmetic.hpp>
 
 #endif
