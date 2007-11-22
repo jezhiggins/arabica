c7c0c3d36c37031f0aec407fab2b7d5b8c68848c
jez <>
2007-11-22 21:24:17 +0000

    xsl:processing-instruction shouldn't allow xml as the name

--- include/XSLT/impl/xslt_processing_instruction.hpp
+++ include/XSLT/impl/xslt_processing_instruction.hpp
@@ -1,6 +1,7 @@
 #ifndef ARABICA_XSLT_PROCESSING_INSTRUCTION_HPP
 #define ARABICA_XSLT_PROCESSING_INSTRUCTION_HPP
 
+#include <text/UnicodeCharacters.hpp>
 #include "xslt_item.hpp"
 
 namespace Arabica
@@ -21,17 +22,37 @@ public:
   virtual void execute(const DOM::Node<std::string>& node, ExecutionContext& context) const
   {
     std::string name = name_->evaluateAsString(node, context.xpathContext());
-    if(name.empty())
-      throw SAX::SAXException("xsl:processing-instruction : name attribute must evaluate to a valid name");
+    validate_name(name);
 
     context.sink().start_processing_instruction(name);
-
     execute_children(node, context);
-
     context.sink().end_processing_instruction();
   } // execute
 
 private:
+  void validate_name(const std::string& name) const
+  {
+    if(name.empty())
+      throw SAX::SAXException("xsl:processing-instruction : name attribute must evaluate to a valid name");
+
+    if(name.length() != 3)
+      return;
+
+    typedef Arabica::text::Unicode<char> UnicodeT;
+
+    if((name[0] != UnicodeT::CAPITAL_X) &&
+       (name[0] != UnicodeT::LOWERCASE_X))
+      return;
+    if((name[1] != UnicodeT::CAPITAL_M) &&
+       (name[1] != UnicodeT::LOWERCASE_M))
+      return;
+    if((name[2] != UnicodeT::CAPITAL_L) &&
+       (name[2] != UnicodeT::LOWERCASE_L))
+      return;
+      
+    throw SAX::SAXException("xsl:processing-instruction name can not be 'xml'");
+  } // validate_name
+
   Arabica::XPath::XPathExpressionPtr<std::string> name_;
 }; // class ProcessingInstruction
 
---
+++ tests/XSLT/testsuite/TESTS/arabica/errors/error05.xsl
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                version="1.0">
+
+  <xsl:output method="xml"/>
+
+  <xsl:template match="/">
+    <xsl:processing-instruction name="xMl">woo</xsl:processing-instruction>
+  </xsl:template>
+
+</xsl:stylesheet>
