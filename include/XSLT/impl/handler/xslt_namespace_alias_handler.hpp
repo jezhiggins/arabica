468d401865ddfe48be1ff5776dc4c3d8b5734f5f
jez <jez@ISLA>
2008-08-08 22:45:53 +0100

    converted tabs to spaces

--- include/XSLT/impl/handler/xslt_namespace_alias_handler.hpp
+++ include/XSLT/impl/handler/xslt_namespace_alias_handler.hpp
@@ -33,17 +33,17 @@ public:
       std::string result_prefix = attrs["result-prefix"];
 
       if(stylesheet_prefix == "#default") 
-	stylesheet_prefix = "";
+        stylesheet_prefix = "";
       if(result_prefix == "#default")
-	result_prefix = "";
+        result_prefix = "";
 
       std::map<std::string, std::string> namespaces = context_.inScopeNamespaces();
       if((namespaces.find(stylesheet_prefix) == namespaces.end()) && 
-	 (!stylesheet_prefix.empty()))
-	throw SAX::SAXException("xslt:namespace-alias " + stylesheet_prefix + " is not a declared namespace prefix");
+         (!stylesheet_prefix.empty()))
+        throw SAX::SAXException("xslt:namespace-alias " + stylesheet_prefix + " is not a declared namespace prefix");
       if((namespaces.find(result_prefix) == namespaces.end()) &&
-	 (!result_prefix.empty()))
-	throw SAX::SAXException("xslt:namespace-alias " + result_prefix + " is not a declared namespace prefix");
+         (!result_prefix.empty()))
+        throw SAX::SAXException("xslt:namespace-alias " + result_prefix + " is not a declared namespace prefix");
 
       context_.addNamespaceAlias(namespaces[stylesheet_prefix], result_prefix, namespaces[result_prefix]);
       return;
