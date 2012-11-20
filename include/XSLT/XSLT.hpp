bdc7db5da858323d1d338f7b936413349d011266
Jez Higgins <jez@jezuk.co.uk>
2012-11-20 08:43:13 +0000

    Put all necessary headers into XSLT.hpp

--- include/XSLT/XSLT.hpp
+++ include/XSLT/XSLT.hpp
@@ -1,6 +1,10 @@
 #ifndef ARABICA_XSLT_XSLT_HPP
 #define ARABICA_XSLT_XSLT_HPP
 
+#include <SAX/InputSource.hpp>
+#include <SAX/Attributes.hpp>
+#include <DOM/SAX2DOM/SAX2DOM.hpp>
+#include <DOM/io/Stream.hpp>
 #include <XSLT/impl/xslt_stylesheet_compiler.hpp>
 
 #endif
