aa21fa7aa0b6ae070e5644df4820c03957deba77
Jez Higgins <jez@jezuk.co.uk>
2015-03-25 17:31:58 +0000

    remove extraneous brackets

--- include/DOM/Simple/DocumentImpl.hpp
+++ include/DOM/Simple/DocumentImpl.hpp
@@ -433,7 +433,7 @@ class DocumentImpl : public DOM::Document_impl<stringT, string_adaptorT>,
 
     virtual DOMNode_implT* removeChild(DOMNode_implT* oldChild)
     {
-      if((documentType_ == oldChild))
+      if(documentType_ == oldChild)
         throw DOM::DOMException(DOM::DOMException::HIERARCHY_REQUEST_ERR);
       DOMNode_implT* result = NodeWithChildrenT::removeChild(oldChild);
  
