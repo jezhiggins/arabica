e8e5846bf258671de87115e9d9057f53c6e50656
Daniel Knibbe <dknibbe@eonerc.rwth-aachen.de>
2016-10-28 15:21:17 +0200

    Added #include <algorithm> for some files in order to compile with MVSC

--- src/convert/utf16beucs2codecvt.cpp
+++ src/convert/utf16beucs2codecvt.cpp
@@ -5,6 +5,8 @@
 #ifndef ARABICA_NO_WCHAR_T
 #include <convert/impl/ucs2_utf16.hpp>
 
+#include <algorithm>
+
 //---------------------------------------------------------------------------
 using namespace Arabica::convert;
 
@@ -49,4 +51,3 @@ int utf16beucs2codecvt::do_length(const std::mbstate_t&,
 
 #endif
 // end of file
-
